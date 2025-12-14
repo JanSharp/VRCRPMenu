c95cc78ffe8869ede8ba48799b8bccf20690c17d
JanSharp <steinmeister36@gmail.com>
2025-12-14 18:20:17 +0100

    Add test UI for creating offline player data

---
+++ Runtime/Dev/TestCreateOfflinePlayerData.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class TestCreateOfflinePlayerData : UdonSharpBehaviour
+    {
+        [HideInInspector][SerializeField][SingletonReference] private PlayerDataManagerAPI playerDataManager;
+
+        public TMP_InputField displayNameField;
+
+        public void OnClick()
+        {
+            playerDataManager.SendCreateOfflinePlayerDataIA(displayNameField.text.Trim());
+        }
+    }
+}

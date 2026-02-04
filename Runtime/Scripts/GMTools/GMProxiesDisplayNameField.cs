befb9451419a6cc0ae6b272e1383538791ecd79b
JanSharp <steinmeister36@gmail.com>
2026-02-04 17:24:49 +0100

    Add clear button for GM proxy display name

---
+++ Runtime/Scripts/GMTools/GMProxiesDisplayNameField.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine.UI;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class GMProxiesDisplayNameField : UdonSharpBehaviour
+    {
+        public TMP_InputField displayNameField;
+        public Button clearButton;
+        public Selectable clearButtonIcon;
+
+        public void OnDisplayNameValueChanged()
+        {
+            UpdateInteractable();
+        }
+
+        public void OnClearClick()
+        {
+            displayNameField.SetTextWithoutNotify("");
+            UpdateInteractable();
+        }
+
+        private void UpdateInteractable()
+        {
+            bool interactable = displayNameField.text != "";
+            clearButton.interactable = interactable;
+            clearButtonIcon.interactable = interactable;
+        }
+    }
+}

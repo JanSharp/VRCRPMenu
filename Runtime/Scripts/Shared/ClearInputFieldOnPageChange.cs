00977c8eb123f24c740cafeb4737c02657031456
JanSharp <steinmeister36@gmail.com>
2026-03-14 15:01:33 +0100

    Clear GM proxy display name field on page change
    But only in VR

---
+++ Runtime/Scripts/Shared/ClearInputFieldOnPageChange.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ClearInputFieldOnPageChange : UdonSharpBehaviour
+    {
+        public TMP_InputField inputField;
+        public bool doClearInVR = true;
+        [Tooltip("Due to the setting which makes the menu automatically change to the home page upon closing "
+            + "the menu, in desktop it's likely generally best not to clear on page change since typing into "
+            + "an input field inherently closes the menu.")]
+        public bool doClearInDesktop = false;
+        private bool isInVR;
+
+        [MenuManagerEvent(MenuManagerEventType.OnMenuManagerStart)]
+        public void OnMenuManagerStart()
+        {
+            isInVR = Networking.LocalPlayer.IsUserInVR();
+        }
+
+        [MenuManagerEvent(MenuManagerEventType.OnMenuActivePageChanged)]
+        public void OnMenuActivePageChanged()
+        {
+            if (isInVR ? doClearInVR : doClearInDesktop)
+                inputField.text = "";
+        }
+    }
+}

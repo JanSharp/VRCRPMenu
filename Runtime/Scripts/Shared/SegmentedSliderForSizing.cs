619c142471193a9b50fafcf2212d0a30f6b6b108
JanSharp <steinmeister36@gmail.com>
2026-03-14 14:38:30 +0100

    Reset size sliders when changing active page

--- Runtime/Scripts/Shared/SegmentedSliderForSizing.cs
+++ Runtime/Scripts/Shared/SegmentedSliderForSizing.cs
@@ -9,7 +9,10 @@ namespace JanSharp
         public SegmentedSlider slider;
         [Min(0f)]
         public float percentagePerSegment = 0.2f;
-        private float middlePointIndex = 0u;
+        /// <summary>
+        /// <para>The index at which th size will be 1.</para>
+        /// </summary>
+        private uint middlePointIndex = 0u;
 
         [MenuManagerEvent(MenuManagerEventType.OnMenuManagerStart)]
         public void OnMenuManagerStart()
@@ -17,9 +20,16 @@ namespace JanSharp
             middlePointIndex = slider.Value;
         }
 
+        [MenuManagerEvent(MenuManagerEventType.OnMenuActivePageChanged)]
+        public void OnMenuActivePageChanged()
+        {
+            // No real need to check if the page changed to or from the page this slider is on.
+            slider.Value = middlePointIndex;
+        }
+
         public float GetSize()
         {
-            return Mathf.Pow(1f + percentagePerSegment, slider.Value - middlePointIndex);
+            return Mathf.Pow(1f + percentagePerSegment, slider.Value - (float)middlePointIndex);
         }
     }
 }
